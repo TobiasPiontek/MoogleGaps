af823da36c8de3adfbef2b25fdf333f055e9936e
TobiasPiontek <tobias_piontek@gmx.de>
2020-04-29 18:15:55 +0200

    offloaded GeoJson element creation to function

--- src/MoogleGaps/GeoJson.java
+++ src/MoogleGaps/GeoJson.java
@@ -1,28 +1,49 @@
 package MoogleGaps;
 
+import com.sun.prism.shader.AlphaOne_LinearGradient_AlphaTest_Loader;
+
 public class GeoJson {
 
     // print GeoJSON of nodeIds
     public static void printGeoJson() {
         System.out.println("{");
         System.out.println("  \"type\": \"FeatureCollection\",");
-        System.out.print("  \"features\": [");
-        for (int i = 0; i < FileReader.nodeIds.size(); i++) {
-            System.out.println("{");
-            System.out.println("      \"type\": \"Feature\",");
-            System.out.println("      \"geometry\": {");
-            System.out.println("        \"type\": \"Point\",");
-            System.out.println("        \"coordinates\": [" + FileReader.longitudes[i] + ", " + FileReader.latitudes[i] + "]");
-            System.out.println("      },");
-            System.out.println("      \"properties\": {");
-            System.out.println("        \"prop0\": \"value0\"");
-            System.out.println("      }");
-            if(i != 99) {
-                System.out.print("    }, ");
+        System.out.println("  \"features\": [");
+        int elementsToPrint = 100;
+        for (int i = 0; i < elementsToPrint; i++) {       // used before: FileReader.nodeIds.size()
+            System.out.println(getGeoJsonElement(6 , FileReader.longitudes[i], FileReader.latitudes[i]));
+
+            if(i != elementsToPrint-1) {
+                System.out.print("}, ");
             } else {
-                System.out.println("  }]");
+                System.out.println("}]");
                 System.out.println("}");
             }
         }
+    }
+
+
+
+    /**
+     * @param moveCharsIn   the static amount of Chars to move in
+     * @param longitude     longitude coordinate of node to display
+     * @param latitude      latitude coordinate of node to display
+     * @return              a GeoJson representation of the coordinates
+     */
+    private static String getGeoJsonElement(int moveCharsIn, double longitude, double latitude){
+        String indention= "";
+        for(int i=0; i<moveCharsIn; i++){
+            indention.concat(" ");
+        }
+        String Element =    "{\n" +
+                            indention + "\"type\": \"Feature\",\n" +
+                            indention + "\"geometry\": {\n" +
+                            indention + "  \"type\": \"Point\",\n" +
+                            indention + "  \"coordinates\": [" + longitude + ", " + latitude + "]\n" +
+                            indention + "},\n" +
+                            indention + "\"properties\": {\n" +
+                            indention + "\"prop0\": \"value0\"\n" +
+                            indention + "}\n";
+        return Element;
     }
 }
