5302c232ccba1f21e04c6a2fb6ae173bba3e6d0b
TobiasPiontek <tobias_piontek@gmx.de>
2020-08-29 20:36:34 +0200

    deleting another useless file

--- Frontend/scripts/script.js
+++
@@ -1,6 +0,0 @@
-function test(){
-    var width = document.getElementById("width").value;
-    var height = document.getElementById("height").value;
-    var multiply=width*height;
-    document.getElementById("answer").value = multiply;
-}
